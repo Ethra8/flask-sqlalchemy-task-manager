bca0e022f671f39f209392d8df4cf42f56fff949
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-03-30 16:17:56 +0000

    update model

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -5,6 +5,7 @@ class Category(db.Model):
     #schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(25), unique=True, nullable=False)  # noqa unique because each category_name must be unique, nullable=False bacause it is a required field
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form  of a string
@@ -15,11 +16,13 @@ class Task(db.Model):
     #schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Column(db.Text, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)  # noqa Text allows textarea with no character restraint
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
-    due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    due_date = db.Column(db.Date, nullable=False)  # noqa for time: DateTime or Time
+    category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)  # ondelete="cascade" : once category_id is deleted, all tasks with same id also get deleted (cascade)
 
     def __repr__(self):
         # __repr__ to represent itself in the form  of a string
-        return self
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
+        )
