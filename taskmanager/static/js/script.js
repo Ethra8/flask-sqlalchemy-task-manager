23cdb1c1ef52d20be3cbfe9cfc256a88b19aa957
Edna TMunill <ethra822@gmail.com>
2024-03-31 19:29:39 +0000

    enable user to add new task

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // sidenav initialization
+    // sidenav initialization - Materialize
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization (add task form) - Materialize
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+    // select initialization
+    var selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+
 });
+
+document.addEventListener('DOMContentLoaded', function () {});
